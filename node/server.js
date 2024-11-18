9e0282b481fb755358e139c2e834f3873e215d80
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-18 17:49:20 +0300

    add env

--- node/server.js
+++ node/server.js
@@ -4,6 +4,8 @@ var cors = require('cors')
 const app = express()
 const Keycloak = require('keycloak-connect')
 
+const APP_ROLE_REPORTS = process.env.APP_ROLE_REPORTS
+
 const keycloak = new Keycloak({})
 keycloak.accessDenied = (_, response) => {
     response.status(401)
@@ -13,7 +15,7 @@ keycloak.accessDenied = (_, response) => {
 app.use(cors())
 app.use(keycloak.middleware())
 
-app.get('/reports', keycloak.protect("realm:prothetic_user"), async (_, res) => {
+app.get('/reports', keycloak.protect(APP_ROLE_REPORTS), async (_, res) => {
     const list = []
     for (var i = 0; i < 10; i++) list.push(randomUUID())
     res.json(list)
