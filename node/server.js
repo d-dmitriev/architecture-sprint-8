2444e65876e3bd8c3c69436997b6cc0c7ee7096e
Dmitry Dmitriev <d.dmitrieff@gmail.com>
2024-11-19 10:03:11 +0300

    format

--- node/server.js
+++ node/server.js
@@ -1,10 +1,11 @@
+const cors = require('cors')
 const { randomUUID } = require('crypto')
 const express = require('express')
-var cors = require('cors')
-const app = express()
 const Keycloak = require('keycloak-connect')
 
 const APP_ROLE_REPORTS = process.env.APP_ROLE_REPORTS
+
+const app = express()
 
 const keycloak = new Keycloak({})
 keycloak.accessDenied = (_, response) => {
@@ -15,10 +16,10 @@ keycloak.accessDenied = (_, response) => {
 app.use(cors())
 app.use(keycloak.middleware())
 
-app.get('/reports', keycloak.protect(APP_ROLE_REPORTS), async (_, res) => {
+app.get('/reports', keycloak.protect(APP_ROLE_REPORTS), async (_, response) => {
     const list = []
     for (var i = 0; i < 10; i++) list.push(randomUUID())
-    res.json(list)
+    response.json(list)
 })
 
 app.listen(8000, () => {
